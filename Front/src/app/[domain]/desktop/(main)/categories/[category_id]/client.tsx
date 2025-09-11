455000522dd2494375c2a46eb7a466ca00f6662f
나현종 <nahj0406@naver.com>
2025-09-11 15:34:30 +0900

    카테고리 폴더 수정

--- Front/src/app/[domain]/desktop/(main)/categories/[category_id]/client.tsx
+++ Front/src/app/[domain]/desktop/(main)/categories/[category_id]/client.tsx
@@ -1,324 +1,245 @@
-"use client";
+"use client"
 import Button from "@/components/buttons/Button";
+import TestProductCard from "@/components/card/TestProductCard";
 import FlexChild from "@/components/flex/FlexChild";
 import HorizontalFlex from "@/components/flex/HorizontalFlex";
 import VerticalFlex from "@/components/flex/VerticalFlex";
 import Image from "@/components/Image/Image";
-import InputNumber from "@/components/inputs/InputNumber";
+import ListPagination from "@/components/listPagination/ListPagination";
+import MasonryGrid from "@/components/masonry/MasonryGrid";
+import NoContent from "@/components/noContent/noContent";
 import P from "@/components/P/P";
 import Span from "@/components/span/Span";
 import clsx from "clsx";
-import { useState } from "react";
+import { usePathname } from "next/navigation";
 import styles from "./page.module.css";
 
-import TestProductCard from "@/components/card/TestProductCard";
-import NoContent from "@/components/noContent/noContent";
-import { Autoplay, Navigation } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
 
-import DeliveryGuide from "./_deliveryGuide/deliveryGuide";
-import Description from "./_description/description";
-import Inquiry from "./_inquiry/inquiry";
-import Review from "./_review/review";
 
-type Option = {
-  name: string;
-  quantity: number;
-  price: string;
-};
 
-// 미니 구매란
-export function MiniInfoBox({ optionTest }: { optionTest: Option[] }) {
-  return (
-    <FlexChild width={"auto"} className={styles.mini_infoBox}>
-      <VerticalFlex>
-        <VerticalFlex gap={20} marginBottom={30}>
-          {optionTest.map((item, i) => (
-            <OptionItem item={item} key={i} />
-          ))}
-        </VerticalFlex>
+export function ProdcutCategory() { // 대분류 카테고리
 
-        <HorizontalFlex className={styles.total_box} gap={10}>
-          <P className={styles.total_txt}>총 상품 금액</P>
+   const pathname = usePathname();
+   
+   // css : 카테고리 추가되어도 flex-wrap 구조 문제 없게 수정하기
 
-          <FlexChild
-            className={styles.price}
-            width={"auto"}
-            justifyContent="end"
-          >
-            <P>25,000</P> ₩
-          </FlexChild>
-        </HorizontalFlex>
+   const ca_test = [
+      {name: '남성토이', thumbnail: '/resources/images/category/gif_ca_Img_01.gif',},
+      {name: '여성토이', thumbnail: '/resources/images/category/gif_ca_Img_02.gif',},
+      {name: '윤활제/젤', thumbnail: '/resources/images/category/gif_ca_Img_03.gif',},
+      {name: '콘돔', thumbnail: '/resources/images/category/gif_ca_Img_04.gif',},
+      {name: '의류', thumbnail: '/resources/images/category/gif_ca_Img_05.gif',},
+      {name: 'BDSM 토이', thumbnail: '/resources/images/category/gif_ca_Img_06.gif',},
+      {name: 'LGBT 토이', thumbnail: '/resources/images/category/gif_ca_Img_07.gif',},
+      {name: '악세서리', thumbnail: '/resources/images/category/ca_img08.png',},
+   ]
 
-        <BuyButtonGroup />
-      </VerticalFlex>
-    </FlexChild>
-  );
+   return (
+      <nav className={styles.category_wrap}>
+         {/* ca_item에 active 클래스 주기. active 클래스만 걸리면 효과 들어감. */}
+         {
+            pathname !== "/" ?
+            <VerticalFlex className={clsx(styles.ca_item, styles.ca_all)}>
+               <FlexChild className={styles.ca_thumb} width={120} height={120}>
+                  <P>ALL</P>
+               </FlexChild>
+              <Span>전체</Span>
+            </VerticalFlex>
+            : null
+         }
+         {
+            ca_test.map((cat, i)=> (
+               <VerticalFlex className={styles.ca_item} key={i}>
+                  <FlexChild className={styles.ca_thumb}>
+                     <Image 
+                        src={cat.thumbnail}
+                        width={'auto'}
+                        height={120}
+                     />
+                  </FlexChild>
+                  <Span>{cat.name}</Span>
+               </VerticalFlex>
+            ))
+         }
+      </nav>
+   )
 }
 
-// 옵션 개수 계산기
-export function OptionItem({ item }: { item: Option }) {
-  return (
-    <HorizontalFlex className={styles.option_item}>
-      <InputNumber />
-      <FlexChild className={styles.txt_item} gap={10} width={"auto"}>
-        <P>{item.name}</P>
-        <FlexChild width={"auto"} gap={5}>
-          <Span>{item.quantity}개</Span>
-          <Span>+ {item.price}원</Span>
-        </FlexChild>
-      </FlexChild>
-    </HorizontalFlex>
-  );
+
+
+export function SecondCategory() { // 중분류, 소분류 카테고리
+   
+
+   const ca_test = [
+      {name: '세척/세정'},
+      {name: '관리/파우더'},
+      {name: '워머/히팅',},
+      {name: '드라이/건조',},
+      {name: '보관/파우치',},
+      {name: '오나홀 보조',},
+      {name: '기타용품',},
+   ]
+
+   return (
+      <>
+         <ul className={styles.category_list}>
+            <li className={styles.active}>
+               <Span>전체</Span>
+            </li>
+            {
+               ca_test.map((cat, i)=> (
+                  <li key={i}>
+                     <Span>{cat.name}</Span>
+                  </li>
+               ))
+            }
+         </ul>
+      </>
+   )
 }
 
-// 좋아요 장바구니 구매버튼 묶음
-export function BuyButtonGroup() {
-  return (
-    <HorizontalFlex className={styles.buyButton_box}>
-      <FlexChild width={"auto"}>
-        <Button className={styles.heart_btn}>
-          <Image
-            src={"/resources/icons/main/product_heart_icon.png"}
-            width={30}
-          />
-          {/* <Image src={'/resources/icons/main/product_heart_icon_active.png'} width={30} /> */}
-        </Button>
-      </FlexChild>
 
-      <FlexChild className={styles.cart_box}>
-        <Button className={styles.cart_btn}>
-          <P>장바구니</P>
-        </Button>
-      </FlexChild>
 
-      <FlexChild className={styles.buy_box}>
-        <Button className={styles.buy_btn}>
-          <P>바로 구매</P>
-        </Button>
-      </FlexChild>
-    </HorizontalFlex>
-  );
+// 인기순, 추천순, 최신순 필터
+export function SortFilter() {
+
+   return (
+      <HorizontalFlex className={styles.sort_group}>
+         <FlexChild className={styles.count_txt}>
+            <P>
+               <b>38</b>개의 상품
+            </P>
+         </FlexChild>
+
+         <FlexChild width={'auto'}>
+            <HorizontalFlex className={styles.sort_box}>
+               <Button className={styles.sort_btn}>인기순</Button>
+               <Button className={styles.sort_btn}>추천순</Button>
+               <Button className={styles.sort_btn}>최신순</Button>
+            </HorizontalFlex>
+         </FlexChild>
+      </HorizontalFlex>
+   )
 }
+
+
+
+
 
 type ListItem = {
-  thumbnail: string;
-  title: string;
-  price: number;
-  discount_rate: number;
-  discount_price: number;
-  heart_count: number;
-  store_name: string;
-  rank: number;
-};
-
-export function ProductSlider({
-  id,
-  lineClamp,
-}: {
-  id: string;
-  lineClamp?: number;
-}) {
-  const ListProduct: ListItem[] = [
-    // 임시
-    {
-      thumbnail: "/resources/images/dummy_img/product_01.png",
-      title: "블랙 골드버스트 바디수트",
-      price: 30000,
-      discount_rate: 12,
-      discount_price: 20000,
-      heart_count: 10,
-      store_name: "키테루 키테루",
-      rank: 0,
-    },
-    {
-      thumbnail: "/resources/images/dummy_img/product_02.png",
-      title: "핑크색 일본 st 로제 베일 가벼움",
-      price: 30000,
-      discount_rate: 12,
-      discount_price: 20000,
-      heart_count: 100,
-      store_name: "키테루 키테루",
-      rank: 1,
-    },
-    {
-      thumbnail: "/resources/images/dummy_img/product_03.png",
-      title: "뒷태 반전 유혹하는 파자마",
-      price: 30000,
-      discount_rate: 12,
-      discount_price: 20000,
-      heart_count: 100,
-      store_name: "키테루 키테루",
-      rank: 2,
-    },
-    {
-      thumbnail: "/resources/images/dummy_img/product_04.jpg",
-      title: "스지망 쿠파 로린코 처녀궁 프리미엄 소프트",
-      price: 30000,
-      discount_rate: 12,
-      discount_price: 20000,
-      heart_count: 70,
-      store_name: "키테루 키테루",
-      rank: 3,
-    },
-    {
-      thumbnail: "/resources/images/dummy_img/product_05.png",
-      title: "[유니더스/얇은콘돔형] 지브라 콘돔 1box(10p) [NR]",
-      price: 30000,
-      discount_rate: 12,
-      discount_price: 20000,
-      heart_count: 4,
-      store_name: "키테루 키테루",
-      rank: 4,
-    },
-    {
-      thumbnail: "/resources/images/dummy_img/product_06.png",
-      title: "블랙 망사 리본 스타킹",
-      price: 30000,
-      discount_rate: 12,
-      discount_price: 20000,
-      heart_count: 1020,
-      store_name: "키테루 키테루",
-      rank: 5,
-    },
-    {
-      thumbnail: "/resources/images/dummy_img/product_07.png",
-      title: "섹시 스트랩 간호사 st 코스튬",
-      price: 30000,
-      discount_rate: 12,
-      discount_price: 20000,
-      heart_count: 1030,
-      store_name: "키테루 키테루",
-      rank: 6,
-    },
-
-    {
-      thumbnail: "/resources/images/dummy_img/product_07.png",
-      title: "섹시 스트랩 간호사 st 코스튬",
-      price: 30000,
-      discount_rate: 12,
-      discount_price: 20000,
-      heart_count: 1030,
-      store_name: "키테루 키테루",
-      rank: 6,
-    },
-
-    {
-      thumbnail: "/resources/images/dummy_img/product_07.png",
-      title: "섹시 스트랩 간호사 st 코스튬",
-      price: 30000,
-      discount_rate: 12,
-      discount_price: 20000,
-      heart_count: 1030,
-      store_name: "키테루 키테루",
-      rank: 6,
-    },
-
-    {
-      thumbnail: "/resources/images/dummy_img/product_07.png",
-      title: "섹시 스트랩 간호사 st 코스튬",
-      price: 30000,
-      discount_rate: 12,
-      discount_price: 20000,
-      heart_count: 1030,
-      store_name: "키테루 키테루",
-      rank: 6,
-    },
-  ];
-
-  return (
-    <>
-      {ListProduct.length > 0 ? (
-        <FlexChild id={id} className={styles.ProductSlider}>
-          <Swiper
-            loop={true}
-            slidesPerView={6}
-            speed={600}
-            spaceBetween={20}
-            modules={[Autoplay, Navigation]}
-            autoplay={{ delay: 4000 }}
-            navigation={{
-              prevEl: `#${id} .${styles.prevBtn}`,
-              nextEl: `#${id} .${styles.nextBtn}`,
-            }}
-          >
-            {ListProduct.map((product, i) => {
-              return (
-                <SwiperSlide key={i}>
-                  <TestProductCard
-                    product={product}
-                    lineClamp={lineClamp ?? 2}
-                  />
-                </SwiperSlide>
-              );
-            })}
-          </Swiper>
-
-          <div className={clsx(styles.naviBtn, styles.prevBtn)}>
-            <Image
-              src={"/resources/icons/arrow/slide_arrow.png"}
-              width={10}
-            ></Image>
-          </div>
-          <div className={clsx(styles.naviBtn, styles.nextBtn)}>
-            <Image
-              src={"/resources/icons/arrow/slide_arrow.png"}
-              width={10}
-            ></Image>
-          </div>
-        </FlexChild>
-      ) : (
-        <NoContent type="상품" />
-      )}
-    </>
-  );
+   thumbnail: string;
+   title: string;
+   price: number;
+   discount_rate: number;
+   discount_price: number;
+   heart_count: number;
+   store_name: string;
+   rank: number;
 }
 
-// 제품 정보 및 내용
-export function DetailTabContainer() {
-  const [tabParams, setTabParams] = useState("description");
-  const tabParamsChange = (params: string) => {
-    setTabParams(params);
-  };
 
-  const tabAraays = [
-    { name: "상세정보", paramsName: "description", component: <Description /> },
-    { name: "사용후기", paramsName: "review", component: <Review /> },
-    { name: "상품 Q&A", paramsName: "inquiry", component: <Inquiry /> },
-    {
-      name: "배송/반품/교환/안내",
-      paramsName: "deliveryGuide",
-      component: <DeliveryGuide />,
-    },
-  ];
+const ListProduct: ListItem[] = [ // 임시
+{
+   thumbnail: '/resources/images/dummy_img/product_01.png',
+   title: '블랙 골드버스트 바디수트',
+   price: 30000,
+   discount_rate: 12,
+   discount_price: 20000,
+   heart_count: 10,
+   store_name: '키테루 키테루',
+   rank: 0,
+},
+{
+   thumbnail: '/resources/images/dummy_img/product_02.png',
+   title: '핑크색 일본 st 로제 베일 가벼움',
+   price: 30000,
+   discount_rate: 12,
+   discount_price: 20000,
+   heart_count: 100,
+   store_name: '키테루 키테루',
+   rank: 1,
+},
+{
+   thumbnail: '/resources/images/dummy_img/product_03.png',
+   title: '뒷태 반전 유혹하는 파자마',
+   price: 30000,
+   discount_rate: 12,
+   discount_price: 20000,
+   heart_count: 100,
+   store_name: '키테루 키테루',
+   rank: 2,
+},
+{
+   thumbnail: '/resources/images/dummy_img/product_04.png',
+   title: '스지망 쿠파 로린코 처녀궁 프리미엄 소프트',
+   price: 30000,
+   discount_rate: 12,
+   discount_price: 20000,
+   heart_count: 70,
+   store_name: '키테루 키테루',
+   rank: 3,
+},
+{
+   thumbnail: '/resources/images/dummy_img/product_05.png',
+   title: '[유니더스/얇은콘돔형] 지브라 콘돔 1box(10p) [NR]',
+   price: 30000,
+   discount_rate: 12,
+   discount_price: 20000,
+   heart_count: 4,
+   store_name: '키테루 키테루',
+   rank: 4,
+},
+{
+   thumbnail: '/resources/images/dummy_img/product_06.png',
+   title: '블랙 망사 리본 스타킹',
+   price: 30000,
+   discount_rate: 12,
+   discount_price: 20000,
+   heart_count: 1020,
+   store_name: '키테루 키테루',
+   rank: 5,
+},
+{
+   thumbnail: '/resources/images/dummy_img/product_07.png',
+   title: '섹시 스트랩 간호사 st 코스튬',
+   price: 30000,
+   discount_rate: 12,
+   discount_price: 20000,
+   heart_count: 1030,
+   store_name: '키테루 키테루',
+   rank: 6,
+},
+]
 
-  return (
-    <>
-      <HorizontalFlex className={styles.tab_wrap}>
-        {tabAraays.map((item) => (
-          <FlexChild
-            key={item.paramsName}
-            className={clsx(
-              styles.content_tab,
-              tabParams === `${item.paramsName}` && styles.active
-            )}
-            onClick={() => tabParamsChange(`${item.paramsName}`)}
-          >
-            <P>
-              {item.name}
-              {["review", "inquiry"].includes(item.paramsName) && (
-                <Span className={styles.list_count}>34</Span> // 리뷰, qna 개수 출력
-              )}
-            </P>
-          </FlexChild>
-        ))}
-      </HorizontalFlex>
+export function CategoryList() {
 
-      <VerticalFlex className={styles.content_view}>
-        <article key={tabParams} className={styles.tab_fade}>
-          {tabAraays.find((t) => t.paramsName === tabParams)?.component}
-        </article>
-      </VerticalFlex>
-    </>
-  );
+   return (
+      <>    
+         {ListProduct.length > 0 ? (
+         <>
+            <SortFilter />
+            <VerticalFlex>
+               <MasonryGrid gap={20} breakpoints={5}>
+                  {
+                     ListProduct.map((product, i) => {
+                        return (
+                           <TestProductCard
+                              product={product}
+                              lineClamp={2}
+                              key={i}
+                              width={244}
+                           />
+                        )
+                     })
+                  }
+               </MasonryGrid>
+            </VerticalFlex>
+         </>
+         ):(
+            <NoContent type={'상품'} />
+         )}
+         
+         <ListPagination />
+      </>
+   )
 }
