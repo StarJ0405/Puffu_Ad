0cd515a6939ed3d7a9143a06ca1671db05b33191
나현종 <nahj0406@naver.com>
2025-09-12 17:35:00 +0900

    404 페이지 퍼블리싱 완료

---
+++ Front/src/app/not-client.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Link from "next/link";
+import useNavigate from "@/shared/hooks/useNavigate";
+import FlexChild from "@/components/flex/FlexChild";
+import P from "@/components/P/P";
+import styles from './not-found.module.css'
+import clsx from "clsx";
+
+export function NaviBtn() {
+   const navigate = useNavigate();
+
+   return (
+      <FlexChild width={'auto'} gap={10}>
+         <FlexChild className={clsx(styles.Back_btn, styles.btn)} onClick={() => navigate(-1)}>
+            <P>이전 페이지</P>
+         </FlexChild>
+
+         <FlexChild className={clsx(styles.home_btn, styles.btn)} onClick={() => navigate('/')}>
+            <P>홈으로</P>
+         </FlexChild>
+      </FlexChild>
+   )
+}
