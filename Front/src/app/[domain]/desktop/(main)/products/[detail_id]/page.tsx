e65ca8637b86d90dd7a633260a9a6738812cad16
StarJ <ghdtjdwo126@gmail.com>
2025-09-15 18:31:05 +0900

    장바구니 담기 및 로그인 엔터처리

--- Front/src/app/[domain]/desktop/(main)/products/[detail_id]/page.tsx
+++ Front/src/app/[domain]/desktop/(main)/products/[detail_id]/page.tsx
@@ -1,23 +1,12 @@
-import Container from "@/components/container/Container";
 import FlexChild from "@/components/flex/FlexChild";
-import HorizontalFlex from "@/components/flex/HorizontalFlex";
 import VerticalFlex from "@/components/flex/VerticalFlex";
-import Image from "@/components/Image/Image";
-import P from "@/components/P/P";
-import Span from "@/components/span/Span";
+import { useAuth } from "@/providers/AuthPorivder/AuthPorivder";
 import { requester } from "@/shared/Requester";
 import clsx from "clsx";
 import { Params } from "next/dist/server/request/params";
 import { notFound } from "next/navigation";
-import {
-  DetailTabContainer,
-  MiniInfoBox,
-  DetailFrame,
-  ProductSlider,
-} from "./client";
+import { ProductSlider, ProductWrapper } from "./client";
 import styles from "./page.module.css";
-import { log } from "@/shared/utils/Functions";
-import { useAuth } from "@/providers/AuthPorivder/AuthPorivder";
 
 export default async function ({ params }: { params: Promise<Params> }) {
   const { detail_id } = await params;
@@ -49,31 +38,20 @@ export default async function ({ params }: { params: Promise<Params> }) {
   // console.log('상품 정보', relationProducts);
 
   return (
-    <section className="root">
-      <Container
-        className={clsx("page_container", styles.detail_container)}
-        marginTop={100}
-      >
-        <DetailFrame initCondition={initCondition} initProduct={initProduct} />
+    <ProductWrapper initCondition={initCondition} initProduct={initProduct}>
+      <VerticalFlex position="relative" marginTop={40} alignItems="start">
+        <FlexChild marginBottom={20}>
+          <h3 className={clsx("SacheonFont", styles.slide_title)}>
+            보시는 상품과 비슷한 추천 상품
+          </h3>
+        </FlexChild>
 
-        <VerticalFlex position="relative" marginTop={40} alignItems="start">
-          <FlexChild marginBottom={20}>
-            <h3 className={clsx("SacheonFont", styles.slide_title)}>
-              보시는 상품과 비슷한 추천 상품
-            </h3>
-          </FlexChild>
-
-          <ProductSlider id={"relation"} lineClamp={1} listArray={relationProducts.content} />
-        </VerticalFlex>
-
-        <HorizontalFlex marginTop={30} alignItems="start" gap={40}>
-          <VerticalFlex className={styles.contents_container} width={850}>
-            <DetailTabContainer initCondition={initCondition} initProduct={initProduct} />
-          </VerticalFlex>
-
-          <MiniInfoBox initCondition={initCondition} initProduct={initProduct} />
-        </HorizontalFlex>
-      </Container>
-    </section>
+        <ProductSlider
+          id={"relation"}
+          lineClamp={1}
+          listArray={relationProducts.content}
+        />
+      </VerticalFlex>
+    </ProductWrapper>
   );
 }
--- Front/src/shared/hooks/useNavigate.tsx
+++ Front/src/shared/hooks/useNavigate.tsx
@@ -1,9 +1,5 @@
 "use client";
-import { useBrowserEvent } from "@/providers/BrowserEventProvider/BrowserEventProviderClient";
-import { usePathname, useRouter } from "next/navigation";
-import { useCookies } from "react-cookie";
-import { Cookies } from "../utils/Data";
-import { getCookieOption } from "../utils/Functions";
+import { useRouter } from "next/navigation";
 
 interface NavigateFunction {
   (to: String, options?: NavigateOptions): void;
@@ -16,51 +12,42 @@ interface NavigateOptions {
 
 export default function useNavigate(): NavigateFunction {
   const router = useRouter();
-  const { setIsReload } = useBrowserEvent();
-  const [cookie, setCookie] = useCookies([Cookies.HISTORY]);
-  const pathname = usePathname();
-
   return (...props) => {
-    if (typeof props?.[0] === "undefined" || props?.[0] === null) return;
-    const scroll = document.getElementById("scroll");
-    if (scroll) {
-      const history = cookie?.[Cookies.HISTORY] || {};
-      history[pathname] = scroll.scrollTop;
-      setCookie(Cookies.HISTORY, history, getCookieOption());
-    }
-    if (typeof props[0] === "number") {
-      const delta: number = props[0];
+    setTimeout(() => {
+      if (typeof props?.[0] === "undefined" || props?.[0] === null) return;
+      if (typeof props[0] === "number") {
+        const delta: number = props[0];
 
-      if (delta === -1) {
-        setIsReload(true);
-        if (window.history.length === 1) router.push("/");
-        else router.back();
-      } else if (delta === 1) {
-        router.forward();
-        setIsReload(true);
-      } else if (delta === 0) {
-        router.refresh();
-        setIsReload(true);
+        if (delta === -1) {
+          if (window.history.length <= 2) router.push("/");
+          else router.back();
+        } else if (delta === 1) {
+          router.forward();
+        } else if (delta === 0) {
+          router.refresh();
+        }
+      } else {
+        const to: string = String(props[0]);
+
+        if (to.split("?")[0] !== window.location.pathname)
+          sessionStorage.removeItem(`scrollPos:${to.split("?")[0]}`);
+        if (props?.[1]) {
+          const option = props?.[1] as NavigateOptions;
+          const _option = {
+            scroll: option.preventScrollReset,
+          };
+          if (option?.type === "new") {
+            window.open(
+              to.startsWith("http")
+                ? to
+                : `${window.location.host}${`/${to}`.replace("//", "/")}`
+            );
+          } else if (option?.type === "replace") {
+            router.replace(to, _option);
+          } else router.push(to, _option);
+        } else router.push(to);
       }
-    } else {
-      setIsReload(false);
-      const to: string = String(props[0]);
-      if (props?.[1]) {
-        const option = props?.[1] as NavigateOptions;
-        const _option = {
-          scroll: option.preventScrollReset,
-        };
-        if (option?.type === "new") {
-          window.open(
-            to.startsWith("http")
-              ? to
-              : `${window.location.host}${`/${to}`.replace("//", "/")}`
-          );
-        } else if (option?.type === "replace") {
-          router.replace(to, _option);
-        } else router.push(to, _option);
-      } else router.push(to);
-    }
+    }, 1);
   };
 }
 
