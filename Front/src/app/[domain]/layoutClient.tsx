f9e38759d0b8583a746ec0ce537bb77e468c5dc6
StarJ <ghdtjdwo126@gmail.com>
2025-09-17 08:53:38 +0900

    로그인 처리

--- Front/src/app/[domain]/layoutClient.tsx
+++ Front/src/app/[domain]/layoutClient.tsx
@@ -1,19 +1,25 @@
 "use client";
 
+import { useAuth } from "@/providers/AuthPorivder/AuthPorivderClient";
 import { AnimatePresence, motion } from "framer-motion";
-import { usePathname } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export default function ({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-
+  const { userData } = useAuth();
+  useEffect(() => {
+    if (!userData?.id && pathname !== "/" && !pathname.startsWith("/auth"))
+      redirect("/auth/login");
+  }, [userData, pathname]);
   return (
     <AnimatePresence mode="wait">
       <motion.div
         id="motion"
         key={pathname}
-        initial={{ opacity: 0,}}
-        // x: -20 
-        animate={{ opacity: 1,}}
+        initial={{ opacity: 0 }}
+        // x: -20
+        animate={{ opacity: 1 }}
         // transition={{ duration: 0.2, ease: "easeInOut" }}
         transition={{ duration: 0.5, ease: "easeInOut" }}
       >
