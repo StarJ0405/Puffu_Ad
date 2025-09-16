e6fee7e85b575cb2a7cd622131ed67dbae080577
StarJ <ghdtjdwo126@gmail.com>
2025-09-16 14:42:53 +0900

    PC 마이페이지 로그아웃

--- Front/src/app/[domain]/desktop/(with_login)/(features)/mypage/client.tsx
+++ Front/src/app/[domain]/desktop/(with_login)/(features)/mypage/client.tsx
@@ -12,37 +12,21 @@ import styles from "./mypage.module.css";
 import Input from "@/components/inputs/Input";
 import ConfirmModal from "@/modals/confirm/ConfirmModal";
 import NiceModal from "@ebay/nice-modal-react";
+import { useState } from "react";
+import { useCookies } from "react-cookie";
+import { Cookies } from "@/shared/utils/Data";
+import { getCookieOption } from "@/shared/utils/Functions";
 
-
-const editInfoModal = () => { // 개인정보 수정
+const editInfoModal = () => {
+  // 개인정보 수정
   NiceModal.show(ConfirmModal, {
     // title: '개인정보 수정',
-    message: (
-      <EditINfo />
-    ),
+    message: <EditINfo />,
     confirmText: "확인",
     withCloseButton: true,
-    onConfirm: async () => {
-      
-    },
-  })
-}
-
-const logoutModal = () => { // 로그아웃
-  NiceModal.show(ConfirmModal, {
-    message: (
-      <FlexChild justifyContent="center" marginBottom={30}>
-        <P color="#333" fontSize={20} weight={600}>로그아웃 하시겠습니까?</P>
-      </FlexChild>
-    ),
-    confirmText: "확인",
-    cancelText: "취소",
-    withCloseButton: true,
-    onConfirm: async () => {
-      
-    },
-  })
-}
+    onConfirm: async () => {},
+  });
+};
 
 export function Profile() {
   return (
@@ -60,9 +44,7 @@ export function Profile() {
         <FlexChild width={"auto"} className={styles.profile_name}>
           <P>콘푸로스트123</P>
 
-          <FlexChild width={'auto'} cursor="pointer"
-            onClick={editInfoModal}
-          >
+          <FlexChild width={"auto"} cursor="pointer" onClick={editInfoModal}>
             <Image
               src={"/resources/icons/mypage/setting_icon.png"}
               width={16}
@@ -75,23 +57,42 @@ export function Profile() {
         <Button>관심 리스트</Button>
       </FlexChild>
     </VerticalFlex>
-  )
+  );
 }
 
 export function MypageNavi() {
+  const [, , removeCookie] = useCookies([Cookies.JWT]);
+  const myshopMenu = [
+    { name: "내 주문 내역", link: "/mypage/myOrders" },
+    { name: "최근 본 상품", link: "/mypage/recentlyView" },
+    { name: "관심리스트", link: "/mypage/wishList" },
+  ];
 
-   const myshopMenu = [
-      { name: "내 주문 내역", link: "/mypage/myOrders" },
-      { name: "최근 본 상품", link: "/mypage/recentlyView" },
-      { name: "관심리스트", link: "/mypage/wishList" },
-   ];
+  const myInfoMenu = [
+    { name: "배송지 관리", link: "/mypage/delivery" },
+    { name: "문의내역", link: "/mypage/inquiry" },
+    { name: "리뷰 관리", link: "/mypage/review" },
+    { name: "회원탈퇴", link: "/mypage/deleteAccount" },
+  ];
 
-   const myInfoMenu = [
-      { name: "배송지 관리", link: "/mypage/delivery" },
-      { name: "문의내역", link: "/mypage/inquiry" },
-      { name: "리뷰 관리", link: "/mypage/review" },
-      { name: "회원탈퇴", link: "/mypage/deleteAccount" },
-   ];
+  const logoutModal = () => {
+    // 로그아웃
+    NiceModal.show(ConfirmModal, {
+      message: (
+        <FlexChild justifyContent="center" marginBottom={30}>
+          <P color="#333" fontSize={20} weight={600}>
+            로그아웃 하시겠습니까?
+          </P>
+        </FlexChild>
+      ),
+      confirmText: "확인",
+      cancelText: "취소",
+      withCloseButton: true,
+      onConfirm: async () => {
+        removeCookie(Cookies.JWT, getCookieOption());
+      },
+    });
+  };
 
   return (
     <VerticalFlex className={clsx(styles.mypage_navi, styles.box_frame)}>
@@ -118,14 +119,13 @@ export function MypageNavi() {
 
         <ul className={styles.inner_menu}>
           <li>
-            <Link className={styles.inner_btn} href={"/mypage/editInfo"}
+            <Link
+              className={styles.inner_btn}
+              href={"/mypage/editInfo"}
               onClick={editInfoModal}
             >
               <Span>개인정보 수정</Span>
-              <Image
-                src={"/resources/icons/arrow/slide_arrow.png"}
-                width={8}
-              />
+              <Image src={"/resources/icons/arrow/slide_arrow.png"} width={8} />
             </Link>
           </li>
 
@@ -141,14 +141,9 @@ export function MypageNavi() {
             </li>
           ))}
           <li>
-            <Link className={styles.inner_btn} href={"/"}
-              onClick={logoutModal}
-            >
+            <Link className={styles.inner_btn} href={"/"} onClick={logoutModal}>
               <Span>로그아웃</Span>
-              <Image
-                src={"/resources/icons/arrow/slide_arrow.png"}
-                width={8}
-              />
+              <Image src={"/resources/icons/arrow/slide_arrow.png"} width={8} />
             </Link>
           </li>
         </ul>
@@ -157,25 +152,36 @@ export function MypageNavi() {
   );
 }
 
-
 export function EditINfo() {
   return (
     <VerticalFlex className="modal_edit_info" gap={50}>
       <FlexChild className="title" justifyContent="center">
-        <P size={25} weight={600}>개인정보 수정</P>
+        <P size={25} weight={600}>
+          개인정보 수정
+        </P>
       </FlexChild>
 
       <VerticalFlex alignItems="start" gap={30}>
-        <VerticalFlex className={'input_box'} alignItems="start" gap={10}>
-          <P size={16} color="#333" weight={600}>아이디</P>
-          <P size={16} color="#797979">mynameistony</P>
+        <VerticalFlex className={"input_box"} alignItems="start" gap={10}>
+          <P size={16} color="#333" weight={600}>
+            아이디
+          </P>
+          <P size={16} color="#797979">
+            mynameistony
+          </P>
         </VerticalFlex>
 
-        <VerticalFlex className={'input_box'} alignItems="start" gap={10}>
-          <P size={16} color="#333" weight={600}>비밀번호</P>
-          <Input type="password" width={'100%'} placeHolder="비밀번호를 입력하세요." />
+        <VerticalFlex className={"input_box"} alignItems="start" gap={10}>
+          <P size={16} color="#333" weight={600}>
+            비밀번호
+          </P>
+          <Input
+            type="password"
+            width={"100%"}
+            placeHolder="비밀번호를 입력하세요."
+          />
         </VerticalFlex>
       </VerticalFlex>
     </VerticalFlex>
-  )
+  );
 }
