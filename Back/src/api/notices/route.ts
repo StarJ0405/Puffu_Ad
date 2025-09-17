0f3e59e8a227191b4293bbaf2c2b021b818713ad
StarJ <ghdtjdwo126@gmail.com>
2025-09-17 15:45:10 +0900

    이벤트

--- Back/src/api/notices/route.ts
+++ Back/src/api/notices/route.ts
@@ -15,6 +15,24 @@ export const GET: ApiHandler = async (req, res) => {
   where.visible = true;
   where.starts_at = Or(IsNull(), LessThanOrEqual(new Date()));
   where.ends_at = Or(IsNull(), MoreThanOrEqual(new Date()));
+  if (where.active) {
+    switch (where.active) {
+      case "before": {
+        where.actives_at = Or(MoreThanOrEqual(new Date()));
+        break;
+      }
+      case "continue": {
+        where.actives_at = Or(IsNull(), LessThanOrEqual(new Date()));
+        where.deactives_at = Or(IsNull(), MoreThanOrEqual(new Date()));
+        break;
+      }
+      case "end": {
+        where.deactives_at = Or(LessThanOrEqual(new Date()));
+        break;
+      }
+    }
+  }
+  delete where.active;
   if (select) {
     if (Array.isArray(select)) {
       if (!select.includes("visible")) select.push("visible");
