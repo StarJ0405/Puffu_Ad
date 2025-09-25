43dbe505d0d8a35bd5f813a75e9874b2549ed5eb
leejaejeong <jaejeonglee6@gmail.com>
2025-09-25 18:48:56 +0900

    pc, 모바일 1:1문의 문의 유형 추가

--- Front/src/app/[domain]/desktop/(main)/(features)/board/inquiry/write/client.tsx
+++ Front/src/app/[domain]/desktop/(main)/(features)/board/inquiry/write/client.tsx
@@ -27,7 +27,7 @@ export function WriteFrame() {
   const { userData } = useAuth();
   const imageRef = useRef<any>(null);
 
-  const [qaType, setQaType] = useState<"etc" | "exchange" | "refund" | "">("");
+  const [qaType, setQaType] = useState<"account" | "order" | "receipt" | "event" | "etc"| "">("");
   const [author, setAuthor] = useState("");
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
@@ -95,14 +95,16 @@ export function WriteFrame() {
                 search: "web_select_search",
               }}
               options={[
-                // { value: "exchange", display: "교환" },
-                // { value: "refund", display: "환불" },
+                { value: "account", display: "회원정보 관리" },
+                { value: "order", display: "주문/결제" },
+                { value: "receipt", display: "영수증/증빙서류" },
+                { value: "event", display: "상품/이벤트" },
                 { value: "etc", display: "기타" },
               ]}
               placeholder={"문의 유형 선택"}
               value={qaType}
               onChange={(value) =>
-                setQaType(value as "etc" | "exchange" | "refund")
+                setQaType(value as "account" | "order" | "receipt" | "event" | "etc" )
               }
             />
           </FlexChild>
