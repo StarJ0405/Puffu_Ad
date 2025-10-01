2403a0f53d7d01ef1a63a2fbef90362968c5845e
leejaejeong <jaejeonglee6@gmail.com>
2025-10-01 14:31:18 +0900

    pc 포인트 내역, 상세ui 모바일 내용으로 복사

--- Front/src/app/[domain]/desktop/(main)/(features)/mypage/point/client.tsx
+++ Front/src/app/[domain]/desktop/(main)/(features)/mypage/point/client.tsx
@@ -8,6 +8,7 @@ import HorizontalFlex from "@/components/flex/HorizontalFlex";
 import Span from "@/components/span/Span";
 import Div from "@/components/div/Div";
 import { useState } from "react";
+import useNavigate from "@/shared/hooks/useNavigate";
 import clsx from "clsx";
 import styles from "./page.module.css";
 
@@ -69,21 +70,22 @@ export function PointHistory({
       date: "2025년 9월 1일",
       title: "[손가락 콘돔] 핑돔 1box 24pcs (Findom 1box) - FD24 (ALC)외 3개",
       point: "1,000",
-      used: true,
+      used: false,
       balance: "9,860",
+      id: "test1",
     },
     {
       date: "2025년 9월 1일",
       title: "[주간할인] 백탁 실리콘 애널 로션",
       point: "1,000",
-      used: true,
+      used: false,
       balance: "11,860",
     },
     {
       date: "2025년 9월 1일",
       title: "초보자 등급 적립금",
-      point: "1,000",
-      used: false,
+      point: "200",
+      used: true,
       balance: "12,060",
     },
   ];
@@ -159,11 +161,13 @@ export function PointHistory({
           <P className={styles.date}>2025.09.01</P>
           {test.map((point, index) => {
             const isUsed = point.used;
+            const navigate = useNavigate();
             return (
               <FlexChild
                 key={index}
                 borderBottom={"1px solid #797979"}
                 paddingBottom={15}
+                onClick={() => navigate(`/mypage/point/${point.id}`)}
               >
                 <HorizontalFlex>
                   <VerticalFlex alignItems="flex-start" gap={10}>
@@ -172,20 +176,15 @@ export function PointHistory({
                   </VerticalFlex>
                   <VerticalFlex alignItems="flex-end" gap={5}>
                     <P className={styles.point}>
-                      <Span>+</Span>
+                      <Span>{isUsed ? '+' : '-'}</Span>
                       <Span>{point.point}</Span>
                       <Span>P</Span>
                     </P>
-                    {isUsed ? (
-                      <P className={styles.status}>
-                      적립
+                    <P className={clsx(styles.status,{
+                        [styles.used] : !isUsed,
+                      })}>
+                      {isUsed ? '적립' : '사용'}
                     </P>
-                    ) : (
-                      <P className={clsx(styles.used, styles.status)}>
-                      사용
-                    </P>
-                    )}
-                    
                     <P className={styles.points_balance}>
                       <Span>잔액 </Span>
                       <Span>{point.balance}</Span>
