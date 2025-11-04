5ba74d0769f9e8a990348b806f6d58380d4e3877
StarJ <ghdtjdwo126@gmail.com>
2025-11-04 15:09:39 +0900

    폰트 수정

--- Front/src/app/admin/desktop/test/page.tsx
+++ Front/src/app/admin/desktop/test/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Editor from "@/components/editor/edtior";
+import Editor, { FontWhitelist } from "@/components/editor/edtior";
 import FlexChild from "@/components/flex/FlexChild";
 import VerticalFlex from "@/components/flex/VerticalFlex";
 
@@ -10,17 +10,22 @@ export default function () {
       <FlexChild backgroundColor="#fff">
         <Editor
           container={[
-            [{ header: [false, 2, 1] }],
-            [{ size: ["small", false, "large", "huge"] }],
-            ["bold", "italic", "underline", "strike"],
-            ["blockquote", "code-block"],
-            [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
-            [{ script: "sub" }, { script: "super" }],
-            [{ indent: "-1" }, { indent: "+1" }],
-            [{ color: [] }, { background: [] }],
-            [{ align: [] }],
-            ["link", "image", "video", "formula"],
-            [{ test: "custom" }],
+            [
+              {
+                font: FontWhitelist,
+              },
+            ],
+            // [{ header: [false, 2, 1] }],
+            // [{ size: ["small", false, "large", "huge"] }],
+            // ["bold", "italic", "underline", "strike"],
+            // ["blockquote", "code-block"],
+            // [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+            // [{ script: "sub" }, { script: "super" }],
+            // [{ indent: "-1" }, { indent: "+1" }],
+            // [{ color: [] }, { background: [] }],
+            // [{ align: [] }],
+            // ["link", "image", "video", "formula"],
+            // [{ test: "custom" }],
           ]}
           modules={{
             test: true,
